f8c3d9c04f0108d3878bd3570fd7f17a37b11aa1
Geani Daniel Cojocaru <36812113+c-daniel14@users.noreply.github.com>
2018-04-05 19:06:22 +0100

    Update OOP Challenge.py

--- OOP Challenge.py
+++ OOP Challenge.py
@@ -1,8 +1,4 @@
-
-# coding: utf-8
-
 # # Object Oriented Programming Challenge
-# 
 # For this challenge, create a bank account class that has two attributes:
 # 
 # * owner
@@ -16,9 +12,6 @@
 # As an added requirement, withdrawals may not exceed the available balance.
 # 
 # Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
-
-# In[33]:
-
 
 class Account:
     def __init__(self,owner,balance):
@@ -37,53 +30,25 @@ class Account:
         else:
             print("Funds Unavailable!")
 
-
-# In[34]:
-
+#############################################################################
 
 # 1. Instantiate the class
 acct1 = Account('Jose',100)
 
-
-# In[35]:
-
-
 # 2. Print the object
 print(acct1)
-
-
-# In[36]:
-
 
 # 3. Show the account owner attribute
 acct1.owner
 
-
-# In[37]:
-
-
 # 4. Show the account balance attribute
 acct1.balance
-
-
-# In[38]:
-
 
 # 5. Make a series of deposits and withdrawals
 acct1.deposit(50)
 
-
-# In[39]:
-
-
 acct1.withdraw(75)
-
-
-# In[40]:
-
 
 # 6. Make a withdrawal that exceeds the available balance
 acct1.withdraw(500)
 
-
-# ## Good job!
